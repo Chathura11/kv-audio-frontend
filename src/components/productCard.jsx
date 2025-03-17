859c5fdbe2ea5d058e590769e63f61cdc9e4ae9e
chathura <chathuraasela11@gmail.com>
2025-03-17 20:43:29 +0530

    modified item page

--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -1,12 +1,15 @@
-import "./productCard.css";
-
-export default function ProductCard(props){
-    return(
-        <div className="product-card">
-            <img src={props.image}/>
-            <span>{props.name}</span>
-            <span>{props.price}</span>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, necessitatibus.</p>
+export default function ProductCard({ item }) {
+    return (
+        <div className="bg-white shadow-lg rounded-lg p-4 m-4 w-[200px] h-[350px] flex flex-col items-center relative">
+            <img src={item.image[0]} alt={item.name} className="w-40 h-30 object-cover rounded-md" />
+            <h2 className="text-lg font-semibold mt-2">{item.name}</h2>
+            <p className="text-gray-500 text-sm">{item.category}</p>
+            <p className="text-gray-700 text-sm mt-1">{item.description}</p>
+            <p className="text-xl font-bold mt-2">{item.price}</p>
+            <p className={`text-sm mt-1 ${item.availability ? 'text-green-500' : 'text-red-500'}`}>
+                {item.availability ? 'In Stock' : 'Out of Stock'}
+            </p>
+            <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 mx-auto absolute bottom-4">View Details</button>
         </div>
-    )
+    );
 }
--- src/pages/home/item.jsx
+++ src/pages/home/item.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import toast from 'react-hot-toast';
+import ProductCard from '../../components/productCard';
 
-function item() {
+export default function Item() {
+
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const [items,setItems] = useState([])
+  const [state,setState] = useState("loading");//loading,success,error
+
+
+  useEffect(()=>{
+    if(state == "loading"){
+      axios.get(`${backendUrl}/api/products`).then((res)=>{
+        setItems(res.data);
+        console.log(res.data)
+        setState("success")
+      }).catch((error)=>{
+        toast.error(error?.response?.data?.message || 'Items loading failed');
+        setState("error")
+      })
+    }
+    
+  },[])
+
   return (
-    <div>item</div>
+    <div className='w-full h-full flex flex-wrap pt-[50px] justify-center'>
+      {state =="loading" &&
+        <div className='w-full h-full  flex justify-center items-center'>
+          <div className='w-[50px] h-[50px] border-4 border-t-green-500 rounded-full animate-spin'></div>
+        </div>
+      }
+      {
+        state =="success" &&
+        items.map((item)=>{
+          return(
+            <ProductCard key={item.key} item={item}/>
+          )
+        })
+      }
+    </div>
   )
 }
-
-export default item
